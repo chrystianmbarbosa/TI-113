d82bafee4a9ec4c6f799240615cf4ee805d65aae
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-09-13 18:19:42 -0300

    fix: Adicionado enunciado do exercício 1

--- funcoes_js/ex1/script.js
+++ funcoes_js/ex1/script.js
@@ -1,4 +1,9 @@
 
+/* Ex1: Crie uma função que lê um vetor de 5 números inteiros e mostre-os, 
+considere que o usuário não pode informar letras, palavras ou vazio. 
+Alerte-o do erro e repita a leitura caso ocorra.
+ */
+
 function lerNumero() {
     let listaNumeros = [];
     for (let i = 0; i < 5; i++) {
